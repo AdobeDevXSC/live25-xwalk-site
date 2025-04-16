7b4032f0e3b080af61c8233297e5e97d02a579da
deck-adobe <deckreyes@adobe.com>
2025-04-16 15:23:00 -0700

    Changes to fstab and cf location

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -6,8 +6,8 @@ import { isAuthorEnvironment, moveInstrumentation } from '../../scripts/scripts.
  * @param {Element} block
  */
 export default async function decorate(block) {
-  const aemauthorurl = 'https://author-p46820-e1400905.adobeaemcloud.com' || '';
-  const aempublishurl = 'https://publish-p46820-e1400905.adobeaemcloud.com' || '';
+  const aemauthorurl = 'https://author-p46907-e1619770.adobeaemcloud.com' || '';
+  const aempublishurl = 'https://publish-p46907-e1619770.adobeaemcloud.com' || '';
   const persistedquery = '/graphql/execute.json/aem-demo-assets/offerByPath';
   const contentPath = block.querySelector(':scope div:nth-child(1) > div a')?.textContent?.trim();
   const variationname =
